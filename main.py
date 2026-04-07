7d8da9a638ada5fd5d19ecfd8ef8b52afcfd21b9
Aarushi0P <aarushi.patangay@education.nsw.gov.au>
2026-04-07 11:35:26 +1000

    Setup Python Flask app and home route

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request
 import db
 
 app = Flask(__name__)
-app.secret_key = "gtg_secret_key_123" # Change this for better security
+app.secret_key = "gtg"
 
 @app.route("/")
 def Home():
-    guessData = db.GetAllGuesses()
-    return render_template("index.html", guesses=guessData)
+    return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+app.run(debug=True, port=5000)
